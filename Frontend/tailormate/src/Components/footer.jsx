453c512246e661f8cb59ac9a41512e2bf402ebf4
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2026-02-05 10:19:12 +0100

    initial setup of react and django apps

---
+++ Frontend/tailormate/src/Components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <p>Made by Makarie Studios</p>
+    </>
+  )
+}
+
+export default Footer
---
+++ Frontend/tailormate/src/Components/landingpage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from "../assets/Logo.png"
+
+const LandingPage = () => {
+  return (
+    <>
+        <div>
+            
+            <img src={Logo} alt="tailormate logo"/>
+        </div>
+        
+        <button>Get Started</button>
+
+    </>
+  )
+}
+
+export default LandingPage
